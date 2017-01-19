f440d54f495f2efb8a32b93e3288fd1787b49efa
Josh Bendavid <Josh.Bendavid@cern.ch>
2017-01-19 08:22:14 +0100

    add option to use PrimaryVertexAssignment in place of PFPileupAlgo for PF(No)Pileup

--- CommonTools/ParticleFlow/python/pfNoPileUpJME_cff.py
+++ CommonTools/ParticleFlow/python/pfNoPileUpJME_cff.py
@@ -6,6 +6,7 @@ from CommonTools.ParticleFlow.goodOfflinePrimaryVertices_cfi import *
 
 pfPileUpJME = pfPileUp.clone( PFCandidates='particleFlowPtrs',
                               Vertices = 'goodOfflinePrimaryVertices',
+                              Jets=cms.InputTag('ak4PFJets'),
                               checkClosestZVertex = False )
 pfNoPileUpJME = pfNoPileUp.clone( topCollection = 'pfPileUpJME',
                                   bottomCollection = 'particleFlowPtrs' )
