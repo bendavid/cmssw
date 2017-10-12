4dea116a54062d497e1522f06eaafd9368e62982
Josh Bendavid <Josh.Bendavid@cern.ch>
2017-10-13 01:26:27 +0200

    add back non-timing vertices as additional collection for phase2_timing_layer era

--- RecoVertex/Configuration/python/RecoVertex_EventContent_cff.py
+++ RecoVertex/Configuration/python/RecoVertex_EventContent_cff.py
@@ -30,6 +30,8 @@ RecoVertexAOD = cms.PSet(
 
 _phase2_tktiming_RecoVertexEventContent = [ 'keep *_offlinePrimaryVertices4D__*',
                                             'keep *_offlinePrimaryVertices4DWithBS__*',
+                                            'keep *_offlinePrimaryVertices1D__*',
+                                            'keep *_offlinePrimaryVertices1DWithBS__*',
                                             'keep *_trackTimeValueMapProducer_*_*' ]
 
 def _phase2_tktiming_AddNewContent(mod):
