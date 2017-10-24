8c6b624108587e7303a5eb6736445ee258999fc8
Josh Bendavid <Josh.Bendavid@cern.ch>
2017-10-24 22:46:59 +0000

    tune pv assignment for packedCandidates in phase 2 and timing scenarios

--- PhysicsTools/PatAlgos/python/slimming/primaryVertexAssociation_cfi.py
+++ PhysicsTools/PatAlgos/python/slimming/primaryVertexAssociation_cfi.py
@@ -8,3 +8,18 @@ primaryVertexAssociation = sortedPFPrimaryVertices.clone(
   produceNoPileUpCollection = cms.bool(False)
 )
 
+from Configuration.Eras.Modifier_phase2_common_cff import phase2_common
+phase2_common.toModify(
+    primaryVertexAssociation,
+    assignment=dict(maxDzErrorForPrimaryAssignment = 999.,
+                    maxDzSigForHighRankedAssignment = 4.0,
+                    ),
+)
+
+from Configuration.Eras.Modifier_phase2_timing_layer_cff import phase2_timing_layer
+phase2_timing_layer.toModify(
+    primaryVertexAssociation,
+    assignment=dict(maxDtSigForHighRankedAssignment = 3.0,
+                    useTiming = True,
+                    ),
+)
