3e10d40d66f35106b7fb579061150d96f5bd6604
Josh Bendavid <Josh.Bendavid@cern.ch>
2017-10-25 02:29:55 +0000

    disable prefer high ranking behaviour for pv assignment

--- PhysicsTools/PatAlgos/python/slimming/primaryVertexAssociation_cfi.py
+++ PhysicsTools/PatAlgos/python/slimming/primaryVertexAssociation_cfi.py
@@ -12,14 +12,12 @@ from Configuration.Eras.Modifier_phase2_common_cff import phase2_common
 phase2_common.toModify(
     primaryVertexAssociation,
     assignment=dict(maxDzErrorForPrimaryAssignment = 999.,
-                    maxDzSigForHighRankedAssignment = 3.0,
                     ),
 )
 
 from Configuration.Eras.Modifier_phase2_timing_layer_cff import phase2_timing_layer
 phase2_timing_layer.toModify(
     primaryVertexAssociation,
-    assignment=dict(maxDtSigForHighRankedAssignment = 3.0,
-                    useTiming = True,
+    assignment=dict(useTiming = True,
                     ),
 )
