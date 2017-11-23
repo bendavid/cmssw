b0370670b7c5dc740320b77c2ccd013add55a46d
Josh Bendavid <Josh.Bendavid@cern.ch>
2017-11-23 22:26:57 +0000

    revert to more standard behaviour for pv assignment of packed candidates

--- PhysicsTools/PatAlgos/python/slimming/primaryVertexAssociation_cfi.py
+++ PhysicsTools/PatAlgos/python/slimming/primaryVertexAssociation_cfi.py
@@ -8,18 +8,9 @@ primaryVertexAssociation = sortedPFPrimaryVertices.clone(
   produceNoPileUpCollection = cms.bool(False)
 )
 
-from Configuration.Eras.Modifier_phase2_common_cff import phase2_common
-phase2_common.toModify(
-    primaryVertexAssociation,
-    assignment=dict(maxDzErrorForPrimaryAssignment = 999.,
-                    maxDzForHighRankedAssignment = 0.1,
-                    ),
-)
-
 from Configuration.Eras.Modifier_phase2_timing_layer_cff import phase2_timing_layer
 phase2_timing_layer.toModify(
     primaryVertexAssociation,
     assignment=dict(useTiming = True,
-                    maxDtSigForHighRankedAssignment = 3.0,
                     ),
 )
